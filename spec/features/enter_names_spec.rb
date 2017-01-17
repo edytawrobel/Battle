48ed8b181dabd008835d2501a1d6c2ceff717730
Eleanor Kavanagh-Brown <ekavanaghbrown@gmail.com>
2017-01-17 17:53:28 +0000

    Challenge 18 almost working

---
+++ spec/features/enter_names_spec.rb
@@ -0,0 +1,13 @@
+require "spec_helper"
+
+# RSpec.feature "Widget management", :type => :feature do
+
+feature 'Enter names' do
+  scenario 'submitting names' do
+    visit('/')
+    fill_in :player_1_name, with: 'Obama'
+    fill_in :player_2_name, with: 'Biden'
+    click_button 'submit'
+    expect(page).to have_content('Obama vs. Biden')
+  end
+end
