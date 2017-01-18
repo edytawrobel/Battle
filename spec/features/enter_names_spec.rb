686d8f2a8d6a515eab934e25a85270420ef1e5ee
Edyta Wrobel <edyta.wrobel.willdoit@gmail.com>
2017-01-18 11:38:37 +0000

    POST/redirect/GET pattern

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -6,8 +6,8 @@ feature 'Enter names' do
   scenario 'submitting names' do
     visit('/')
     fill_in :player_1_name, with: 'Obama'
-    fill_in :player_2_name, with: 'Biden'
+    fill_in :player_2_name, with: 'Anna'
     click_button 'submit'
-    expect(page).to have_content('Obama vs. Biden')
+    expect(page).to have_content('Obama vs. Anna')
   end
 end
