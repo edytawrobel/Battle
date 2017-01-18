ad182e19864283192ba52502107128db70674f9b
Stefan Liute <stefanliute@gmail.com>
2017-01-18 13:52:43 +0000

    Implementing hit points

--- lib/player.rb
+++ lib/player.rb
@@ -1,10 +1,18 @@
 class Player
+  HIT_POINTS = 100
+  attr_reader :name, :hit_points
 
-  attr_reader :name
-
-  def initialize(name)
+  def initialize(name, hit_points = HIT_POINTS)
     @name = name
+    @hit_points = hit_points
   end
 
+  def attack(player)
+    player.receive_damage
+  end
+
+  def receive_damage
+    @hit_points -= 10
+  end
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,11 +1,33 @@
 require 'player'
 
 describe Player do
-  let(:name) { "Obama" }
-  subject(:player) { described_class.new(name) }
+  subject(:player_1) { described_class.new("Obama") }
+  subject(:player_2) { described_class.new("Anna") }
 
-
-  it "returns player's name" do
-    expect(player.name).to eq(name)
+  describe '#name' do
+    it "returns players' names" do
+      expect(player_1.name).to eq("Obama")
+      expect(player_2.name).to eq("Anna")
+    end
   end
+
+  describe '#hit_points' do
+    it "begins with constant-defined value" do
+      expect(player_1.hit_points).to eq(described_class::HIT_POINTS)
+    end
+  end
+
+  describe '#attack' do
+    it "hits opponent" do
+      expect(player_2).to receive(:receive_damage)
+      player_1.attack(player_2)
+    end
+  end
+
+  describe '#receive_damage' do
+    it "reduces score when attacked by opponent" do
+      expect { player_1.attack(player_2) }.to change { player_2.hit_points }.by(-10)
+    end
+  end
+
 end
