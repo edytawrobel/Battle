3476a71ec1ec2c124bb540c729503571abed671a
Stefan Liute <stefanliute@gmail.com>
2017-01-18 11:48:34 +0000

    Viewing hit points

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -11,3 +11,13 @@ feature 'Enter names' do
     expect(page).to have_content('Obama vs. Anna')
   end
 end
+
+feature "See how close I am to winning" do
+  scenario "by seeing opponent's hit points" do
+    visit('/')
+    fill_in :player_1_name, with: 'Obama'
+    fill_in :player_2_name, with: 'Anna'
+    click_button 'submit'
+    expect(page).to have_content('Anna has 100 hit points.')
+  end
+end
