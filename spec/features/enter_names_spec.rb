076e5df3ee4bafd975066ba90fbde51da984a51e
Edyta Wrobel <edyta.wrobel.willdoit@gmail.com>
2017-01-18 11:53:22 +0000

    Add test helpers

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -4,20 +4,14 @@ require "spec_helper"
 
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_1_name, with: 'Obama'
-    fill_in :player_2_name, with: 'Anna'
-    click_button 'submit'
+    sign_in_and_play
     expect(page).to have_content('Obama vs. Anna')
   end
 end
 
 feature "See how close I am to winning" do
   scenario "by seeing opponent's hit points" do
-    visit('/')
-    fill_in :player_1_name, with: 'Obama'
-    fill_in :player_2_name, with: 'Anna'
-    click_button 'submit'
+    sign_in_and_play
     expect(page).to have_content('Anna has 100 hit points.')
   end
 end
