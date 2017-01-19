3ed8103ed3f9952676eb6cfca4e8d030b483b992
Edyta Wrobel <edyta.wrobel.willdoit@gmail.com>
2017-01-19 23:25:42 +0000

    CH.28 (WIP) - see my own hit points

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-feature "Win a game by attacking opponent" do
+feature "Win a game by attacking opponent - Player2" do
   scenario "and get confirmation" do
     sign_in_and_play
     click_button 'attack'
@@ -8,7 +8,19 @@ feature "Win a game by attacking opponent" do
   end
 end
 
-feature "Attack an opponent" do
+# As Player 1,
+# So I can lose a game of Battle,
+# I want Player 2 to attack me, and I want to get a confirmation
+
+feature "Player1 loses a game of Battle by Player2 attacking" do
+  scenario "and getting a confirmation" do
+    sign_in_and_play
+    click_button 'attack'
+    expect(page).to have_content('Anna is attacking Obama')
+  end
+end
+
+feature "Attack an opponent - Player2" do
   scenario "and reduce their hit points" do
     sign_in_and_play
     click_button 'attack'
@@ -17,6 +29,10 @@ feature "Attack an opponent" do
     expect(page).to have_content('Anna has 90 hit points.')
   end
 end
+
+# As Player 1,
+# So I can start to lose a game of Battle,
+# I want Player 2's attack to reduce my HP
 
 feature "switch turns" do
   scenario "after attacking" do
