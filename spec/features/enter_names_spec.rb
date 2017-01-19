e1ae3559f8c24a73bdc86aead375201547c9f93b
Edyta Wrobel <edyta.wrobel.willdoit@gmail.com>
2017-01-19 22:46:36 +0000

    implement a common html across the views

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -15,3 +15,20 @@ feature "See how close I am to winning" do
     expect(page).to have_content('Anna has 100 hit points.')
   end
 end
+
+feature "see how close I am to losing" do
+  scenario "by seeing my own hit points" do
+    sign_in_and_play
+    expect(page).to have_content('Obama has 100 hit points')
+  end
+end
+
+
+
+# As Player 1,
+# So I can lose a game of Battle,
+# I want Player 2 to attack me, and I want to get a confirmation
+#
+# As Player 1,
+# So I can start to lose a game of Battle,
+# I want Player 2's attack to reduce my HP
